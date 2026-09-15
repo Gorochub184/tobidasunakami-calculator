9cbd763fd87fc57a5636d9cc7afd2531b4e40789
Gorochub184 <kouki.naka.0025@gmail.com>
2026-09-15 17:58:07 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request
+from keisan import doryokutiyosou
+from keisan import hpyosou
+from keisan import kaihukuryouyosou
+from pokemon_data import get_hp_stat
+from pokemon_data import get_attack_stat
+from pokemon_name import pokemon_dict
+from fractions import Fraction
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template(
+        "index.html",
+        pokemon_names=list(pokemon_dict.keys())
+    )
+
+
+@app.route("/calculate/doryokuti", methods=["POST"])
+def calculate_doryokuti():
+    pokemon_name = request.form["pokemonName"]
+    hpwariaimae = int(request.form["beforeHP"])
+    hpwariaiato = int(request.form["afterHP"])
+    nokorihp = int(request.form["nokoriHP"])
+    
+    hshuzokuti = get_hp_stat(pokemon_name)
+
+    result = doryokutiyosou(
+        hshuzokuti,
+        hpwariaimae,
+        hpwariaiato,
+        nokorihp
+    )
+    print("努力値候補: ", result)
+    return str(result)
+
+
+@app.route("/calculate/hp", methods=["POST"])
+def calculate_hp():
+    pokemon_name = request.form["pokemonName2"]
+    hdoryokuti = int(request.form["hdoryokuti"])
+    hpwariai = int(request.form["hpwariai"])
+    
+    hshuzokuti = get_hp_stat(pokemon_name)
+    
+    result = hpyosou(
+        hshuzokuti,
+        hdoryokuti,
+        hpwariai
+    )
+    print("残りHP:", result)
+    return str(result)
+
+
+@app.route("/calculate/kaihukuryou", methods=["POST"])
+def calculate_kaihukuryou():
+    pokemon_name = request.form["pokemonName3"]
+    adoryokuti = int(request.form["adoryokuti"])
+    ahosei = float(request.form["ahosei"])
+    aranku = Fraction(request.form["aranku"])
+    
+    ashuzokuti = get_attack_stat(pokemon_name)
+    
+    result = kaihukuryouyosou(
+        ashuzokuti,
+        adoryokuti,
+        ahosei,
+        aranku
+    )
+    print("回復量:", result)
+    return str(result)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ keisan.py
@@ -0,0 +1,97 @@
+#hshuzokuti 相手のh種族値
+#hpwariaimae 飛び出す中身を受ける前のhp割合
+#hpwariaiato 飛び出す中身を受けた後のhp割合
+#nokorihp ウツボットの残りhp
+
+def doryokutiyosou(hshuzokuti, hpwariaimae, hpwariaiato, nokorihp):
+
+    if hpwariaimae < 0 or hpwariaimae > 100 or hpwariaiato < 0 or hpwariaiato > 100 or nokorihp < 0:
+        return []
+
+    candidates = []
+
+    for hdoryokuti in range(33):
+
+        hzissuuti = hshuzokuti + 75 + hdoryokuti  # hp実数値
+
+        # %表示になりうるHP範囲
+        hm = hzissuuti * hpwariaimae // 100
+        ha = hzissuuti * (hpwariaimae + 1) // 100
+        ha = min(ha, hzissuuti)
+
+        for hontouhp in range(hm, ha + 1):
+
+            # 本当の%表示か確認
+            hw = hontouhp * 100 // hzissuuti
+
+            if hw != hpwariaimae:
+                continue
+
+            # 飛び出す中身ダメージ
+            damezi = hontouhp - nokorihp
+
+            if damezi < 0:
+                continue
+
+            # ダメージ後表示
+            dameziwariai = damezi * 100 // hzissuuti
+
+            if dameziwariai == hpwariaiato:
+
+                # まだ保存されていなければ追加
+                if hdoryokuti not in candidates:
+                    candidates.append(hdoryokuti)
+
+    return candidates
+
+
+#hshuzokuti2 相手のh種族値
+#hdoryokuti2 相手のh努力値
+#hpwariai 相手のhp割合
+
+def hpyosou(hshuzokuti2, hdoryokuti2, hpwariai):
+
+    if hdoryokuti2 < 0 or hdoryokuti2 > 32 or hpwariai < 0 or hpwariai > 100:
+        return []
+
+    candidates = []
+
+    #h実数値計算
+    hzissuuti = int(hshuzokuti2 + 75 + hdoryokuti2)
+
+    # %表示になりうるHP範囲
+    hm = hzissuuti * hpwariai // 100
+    ha = hzissuuti * (hpwariai + 1) // 100
+    ha = min(ha, hzissuuti)
+
+    for hontouhp in range(hm, ha + 1):
+
+        # 本当の%表示か確認
+        hw = hontouhp * 100 // hzissuuti
+
+        if hw != hpwariai:
+            continue
+
+        # まだ保存されていなければ追加
+        if hontouhp not in candidates:
+            candidates.append(hontouhp)
+            
+    return candidates
+
+
+#ashuzokuti 相手のa種族値
+#adoryokuti 相手のa努力値
+#ahosei 相手のa補正
+#aranku 相手のaランク
+
+def kaihukuryouyosou(ashuzokuti, adoryokuti, ahosei, aranku):
+    
+    if adoryokuti < 0 or adoryokuti > 32:
+        return []
+    #a実数値計算
+    azissuuti = int((ashuzokuti + 20 + adoryokuti) * ahosei)
+
+    aa = int(azissuuti * aranku)
+
+    print(aa)
+    return aa
