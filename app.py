80728326e5fb3db39042d3637cd8bebb24c7eda9
Gorochub184 <kouki.naka.0025@gmail.com>
2026-09-17 13:35:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -72,5 +72,10 @@ def calculate_kaihukuryou():
     return str(result)
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
